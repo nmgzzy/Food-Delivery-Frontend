dfd3409a487c33f03688f0b14dbf6cbb256e3f89
Zeyu Zhao <283003556@qq.com>
2022-03-13 00:34:26 +0000

    3.13 zzy add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <p>hello</p>
+        <ul>
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/login">login</Link></li>
+        </ul>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
