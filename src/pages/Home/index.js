4e85a6cf7675c9ef3b258f597c8f4f49e1f39ee5
Zeyu Zhao <283003556@qq.com>
2022-04-16 00:46:19 +0100

    4.16 zzy login, signup ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+// import ResponsiveAppBar from '../../components/ResponsiveAppBar';
+// import IconMenu from '../../components/IconMenu';
+// import MyCarousel from '../../components/MyCarousel';
+
+import './home.css';
 
 export default function Home() {
     return (
-        <p>home</p>
+        <div>
+            <p>123</p>
+            {/* <ResponsiveAppBar/>
+            <br/>
+            <div className='menuandpic'>
+                <div className='menuandpiciterm1'><IconMenu /></div>
+                <div className='menuandpiciterm2'><MyCarousel /></div>
+                <div className='menuandpiciterm3'><IconMenu /></div>
+                
+            </div> */}
+        </div>
     );
 }
